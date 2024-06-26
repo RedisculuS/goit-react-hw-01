0539de108b6eebcffde598ef5b4a2a08202d9537
RedisculuS <Altunina311@gmail.com>
2024-06-26 14:53:56 +0300

    2nd commit
    Task 1 without css done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,63 +1,30 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+// import { Mailbox } from './Mailbox';
+// import { Product } from './Product';
+// import { BookList, favouriteBooks } from './BookList';
+// import Card from './Card';
+// import Alert from './Alert';
 
-// function App() {
-//   const [count, setCount] = useState(0)
+// import { HiUser } from 'react-icons/hi';
 
+// export const UserMenu = ({ name }) => {
 //   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
+//     <div>
+//       <p>
+//         <HiUser className="my-icon" size="24" /> {name}
 //       </p>
-//     </>
-//   )
-// }
+//     </div>
+//   );
+// };
 
-// export default App
+// export default function App() {
+//   return (
+//     <div>
+{
+  /* <h1>Best selling</h1> */
+}
 
-import { Mailbox } from './Mailbox';
-import { Product } from './Product';
-import { BookList, favouriteBooks } from './BookList';
-import Card from './Card';
-import Alert from './Alert';
-
-import { HiUser } from 'react-icons/hi';
-
-export const UserMenu = ({ name }) => {
-  return (
-    <div>
-      <p>
-        <HiUser className="my-icon" size="24" /> {name}
-      </p>
-    </div>
-  );
-};
-
-export default function App() {
-  return (
-    <div>
-      <h1>Best selling</h1>
-
-      <Product
+{
+  /* <Product
         name="Tacos With Lime"
         imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
         price={10.99}
@@ -66,8 +33,10 @@ export default function App() {
         name="Fries and Burger"
         imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
         price={14.29}
-      />
-
+      /> */
+}
+{
+  /* 
       <Mailbox username="Natalie" messages={('hello', 'message1')} />
 
       <h1>Books of the week</h1>
@@ -91,7 +60,33 @@ export default function App() {
         Please update your profile contact information
       </Alert>
 
-      <UserMenu></UserMenu>
-    </div>
+      <UserMenu></UserMenu> */
+}
+//       <Profile
+//         name={userData.username}
+//         tag={userData.tag}
+//         location={userData.location}
+//         image={userData.avatar}
+//         stats={userData.stats}
+//       />
+//     </div>
+//   );
+// }
+
+import Profile from './Profile';
+import userData from '../userData.json';
+
+export default function App() {
+  return (
+    <>
+      {' '}
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />{' '}
+    </>
   );
 }
